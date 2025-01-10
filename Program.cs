28d9c8140d62aec9c8d256b23005c78a1368ef81
Njf092024 <Njf092024@gmail.com>
2025-01-10 09:08:37 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -7,5 +7,5 @@ var guide = new Guide();
 /* Her sier vi hvilken oppgaveliste som skal brukes. */
 var assignments = new AssignmentList();
 /* Her går vi gjennom oppgavesettet. Programmet krasjer med vilje on Assert Failure, men også starte på nytt ved en filsave.
-    Når programmet starter på nytt blir en ny rapport generert i consol, med feedback på hva assignments som er ferdig, og hva som er neste på listen. */
+    Når programmet starter på nytt blir en ny rapport generert i consol, med feedback på hva assignments som er ferdig, og hva som er neste på listen.. */
 return assignments.Walkthrough(guide);
